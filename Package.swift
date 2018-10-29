374ae3e20a85a8485f6ed499355d6ec1c7d4f6e4
Noah Emmet <noah@noahemmet.com>
2018-10-29 16:13:33 -0700

    point to master

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
 
     dependencies: [
-    .package(url: "https://github.com/noahemmet/GraphQL.git", .branch("swift-42")),
+    .package(url: "https://github.com/noahemmet/GraphQL.git", .branch("master")),
     ],
 
     targets: [
